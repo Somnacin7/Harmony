9586ebccdf42b32029b447ec07123f543ea6f5d3
somn <somnacin7@gmail.com>
2017-09-06 14:12:53 -0400

    .

--- Assets/Scripts/DotCircle.cs
+++ Assets/Scripts/DotCircle.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class DotCircle : MonoBehaviour {
 
-	// Use this for initialization
+    public GameObject dot = null;
+    public float radius = 1.0f;
+    public float radiusRange = 1.0f;
+    public float frequency = 1.0f;
+    public int numberOfDots = 10;
+
+    private float _step;
+    private List<GameObject> _dots = new List<GameObject>();
+
 	void Start () {
-		
+        _step = (2 * Mathf.PI) / numberOfDots;
+        float halfPi = Mathf.PI / 2;
+
+		for (float i = 0; i < (2 * Mathf.PI); i += _step)
+        {
+            var offset = new Vector3(Mathf.Cos(i + halfPi), Mathf.Sin(i + halfPi));
+            var tmp = radius + Mathf.Sin(i * frequency) * radiusRange;
+            offset *= tmp;
+
+            _dots.Add(Instantiate(dot, transform.position + offset, Quaternion.identity, null));
+        }
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
