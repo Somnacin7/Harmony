b3ea35829ad3fb8d295d9d5c68798ad8706eaac1
Somnacin7 <somnacin7@gmail.com>
2017-09-06 13:48:44 -0400

    add soundgenerator and waveform file

---
+++ Assets/DrawWaveform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SoundGenerator))]
+public class DrawWaveform : MonoBehaviour {
+
+    public GameObject dot;
+
+    private SoundGenerator soundGenerator;
+
+    private void Awake()
+    {
+        soundGenerator = GetComponent<SoundGenerator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
